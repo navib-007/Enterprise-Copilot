22cf3a281a35228f2df14676f6d26548f228566b
Navin <navinyab95@gmail.com>
2026-06-13 00:44:36 +0530

    updated .gitignore

--- app/tests/database/test_base.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import SQLModel
-
-from app.database.base import get_metadata
-
-
-def test_get_metadata_returns_sqlmodel_metadata() -> None:
-    assert get_metadata() is SQLModel.metadata
